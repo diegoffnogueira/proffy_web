19c5cf7c30548b681bc5486d3187fcdeb0f2cfe9
Diego Nogueira <diego.nogueira@m4u.com.br>
2020-08-17 18:59:41 -0300

    Creating forms screen for class registration

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
 
@@ -10,91 +10,107 @@ import Select from "../../components/Select";
 
 
 function TeacherForm() {
-    return(
-        <div id="page-teacher-form" className="container">
-            <PageHeader title="Que ínclrivel que você quer dar aulas."
-            description="O primeiro passo é preencher esse formulário de inscrição"/>
 
-            <main>
-              <fieldset>
+  const [scheduleItems, setScheduleItems] = useState([
+    {week_day: 0, from: '', to: ''}
+  ])
 
-                <legend>Seus dados</legend>
-                <Input name="name" label="Nome completo"/>
-                <Input name="avatar" label="Avatar"/>
-                <Input name="whatsapp" label="Whatsapp"/>
-                <Textarea name="bio" label="Biografia"/>
+  function addNewScheduleItem() {
+    setScheduleItems([
+      ...scheduleItems,
+      {week_day: 0, from: '', to: ''}
+    ]);
+  }
 
-              </fieldset>
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que ínclrivel que você quer dar aulas."
+                  description="O primeiro passo é preencher esse formulário de inscrição"/>
 
-              <fieldset>
+      <main>
+        <fieldset>
 
-                <legend>Sobre a aula</legend>
+          <legend>Seus dados</legend>
+          <Input name="name" label="Nome completo"/>
+          <Input name="avatar" label="Avatar"/>
+          <Input name="whatsapp" label="Whatsapp"/>
+          <Textarea name="bio" label="Biografia"/>
+
+        </fieldset>
+
+        <fieldset>
+
+          <legend>Sobre a aula</legend>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              {value: 'Artes', label: 'Artes'},
+              {value: 'Biologia', label: 'Biologia'},
+              {value: 'Ciências', label: 'Ciências'},
+              {value: 'Física', label: 'Física'},
+              {value: 'Geografia', label: 'Geografia'},
+              {value: 'Matemática', label: 'Matemática'},
+              {value: 'Português', label: 'Português'},
+              {value: 'Química', label: 'Química'},
+              {value: 'Ed. Física', label: 'Ed. Física'},
+              {value: 'História', label: 'História'}
+            ]}
+          />
+
+          <Input name="cost" label="Custo da sua aula por hora"/>
+
+        </fieldset>
+
+        <fieldset>
+
+          <legend>
+            Horários disponíveis
+            <button type="button" onClick={addNewScheduleItem}>
+              + Novo horário
+            </button>
+          </legend>
+
+          {scheduleItems.map(scheduleItem => {
+            return (
+              <div key={scheduleItem.week_day} className="schedule-item">
+
                 <Select
-                  name="subject"
-                  label="Matéria"
+                  name="week_day"
+                  label="Dia da semana"
                   options={[
-                    { value: 'Artes', label: 'Artes' },
-                    { value: 'Biologia', label: 'Biologia' },
-                    { value: 'Ciências', label: 'Ciências' },
-                    { value: 'Física', label: 'Física' },
-                    { value: 'Geografia', label: 'Geografia' },
-                    { value: 'Matemática', label: 'Matemática' },
-                    { value: 'Português', label: 'Português' },
-                    { value: 'Química', label: 'Química' },
-                    { value: 'Ed. Física', label: 'Ed. Física' },
-                    { value: 'História', label: 'História' }
+                    {value: '0', label: 'Domingo'},
+                    {value: '1', label: 'Segunda-feira'},
+                    {value: '2', label: 'Terça-feira'},
+                    {value: '3', label: 'Quarta-feira'},
+                    {value: '4', label: 'Quinta-feira'},
+                    {value: '5', label: 'Sexta-feira'},
+                    {value: '6', label: 'Sábado'},
                   ]}
                 />
 
-                <Input name="cost" label="Custo da sua aula por hora"/>
-                
-              </fieldset>
+                <Input name="from" label="Das" type="time"/>
+                <Input name="to" label="Até" type="time"/>
 
-              <fieldset>
+              </div>
+            );
+          })}
 
-                <legend>
-                  Horários disponíveis
-                  <button type="button">
-                    + Novo horário
-                  </button>
-                </legend>
+        </fieldset>
 
-                <div className="schedule-item">
-                  
-                  <Select
-                    name="week_day"
-                    label="Dia da semana"
-                    options={[
-                      { value: '0', label: 'Domingo' },
-                      { value: '1', label: 'Segunda-feira' },
-                      { value: '2', label: 'Terça-feira' },
-                      { value: '3', label: 'Quarta-feira' },
-                      { value: '4', label: 'Quinta-feira' },
-                      { value: '5', label: 'Sexta-feira' },
-                      { value: '6', label: 'Sábado' },
-                    ]}
-                  />
-                  
-                  <Input name="from" label="Das" type="time"/>
-                  <Input name="to" label="Até" type="time"/>
-
-                </div>
-
-              </fieldset>
-
-              <footer>
-                <p>
-                  <img src={warningIcon} alt="Aviso importante" />
-                  Importante! <br/>
-                  Preencha todos os dados
-                </p>
-                <button type="button">
-                  Salvar cadastro
-                </button>
-              </footer>
-            </main>
-        </div>
-    )
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante"/>
+            Importante! <br/>
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+      </main>
+    </div>
+  )
 }
 
 export default TeacherForm;
